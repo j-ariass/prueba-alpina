3a46b7a4f5d1d39dca21e30fcfe8c3e0ebf05c31
j-ariass <109548307+j-ariass@users.noreply.github.com>
2022-07-18 13:20:58 -0500

    Add files via upload

---
+++ modulo1.js
@@ -0,0 +1,3 @@
+function open_mod2() {
+window.open(https://academiacomercialalpina.com/cursos/sco/41/27/162/1);
+}
---
+++ modulo2.js
@@ -0,0 +1,3 @@
+function open_mod3() {
+window.open(https://academiacomercialalpina.com/cursos/sco/41/28/162/1);
+}
